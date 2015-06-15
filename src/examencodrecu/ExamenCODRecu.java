212b274263ee70d597cd50e1d42e97b2bfe3aa0d
Marcos <Marcos@Marcos>
2015-06-15 10:42:16 +0200

    main

--- src/examencodrecu/ExamenCODRecu.java
+++ src/examencodrecu/ExamenCODRecu.java
@@ -5,6 +5,9 @@
  */
 package examencodrecu;
 
+import javax.swing.JOptionPane;
+import twitter4j.TwitterException;
+
 /**
  *
  * @author Marcos
@@ -14,9 +17,39 @@ public class ExamenCODRecu {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
 
-    
+      String busqueda;
+        String tweet;
+        int opcion;
+        
+        
+        
+        MetodosTwitter obj = new MetodosTwitter();
+        
+         do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("opciones: \n1 --> TimeLine\n2 --> Buscar\n3 --> Tweettear\n4 --> Exit"));
+            switch (opcion) {
+                case 1:
+                    obj.verLineaTiempo();
+                    break;
+                case 2:
+                    busqueda = JOptionPane.showInputDialog("Introduzca lo que desee buscar");
+                    obj.buscarTuit(busqueda);
+                    break;
+                case 3:
+                    tweet = JOptionPane.showInputDialog("Introduzca lo que desee twittear");
+                    obj.twittear(tweet);
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.exit(0);
+            }
+        } while (opcion != 0 && opcion != 4);
+    }
+        
+        
     }
     
-}
+
